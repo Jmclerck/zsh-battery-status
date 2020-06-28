427cd60758849b8f3693233093d59263777933da
Jon Clerck <j.m.clerck@icloud.com>
2020-06-28 22:00:52 +0100

    Initial commit 🎉
    Signed-off-by: Jon Clerck <j.m.clerck@icloud.com>

---
+++ zsh-battery-status.zsh
@@ -0,0 +1,17 @@
+function battery() {
+  local state=$(pmset -g batt)
+  local charging=$(echo $state | rg -o "\scharg[ing|ed]")
+  local discharging=$(echo $state | rg -o "\sdischarging")
+
+  if [[ -n $charging ]]; then
+    local time=$(echo $state | rg -o "[0-9]:[0-9]*")
+    
+    NEXT_BATTERY_STATUS=" $time"
+  elif [[ -n $discharging ]]; then
+    local icons=("" "" "" "" "")
+    local percentage=$(echo $state | rg -o "[0-9]*%" | rg -o "[0-9]*")
+    local segment=$(( ($percentage / 20) + 1 ))
+    
+    NEXT_BATTERY_STATUS="${icons[$segment]}$percentage%%"
+  fi
+}
