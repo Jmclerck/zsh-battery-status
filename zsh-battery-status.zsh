8df48995178f29a8f1bda43bc09c47bed5cbe0e1
Jonathan Clerck <j.m.clerck@icloud.com>
2021-10-22 17:04:08 +0100

    Replace ripgrep

--- zsh-battery-status.zsh
+++ zsh-battery-status.zsh
@@ -1,5 +1,5 @@
 function battery_status_charging() {
-  local time=$(echo $state | rg -o "[0-9]:[0-9]*")
+  local time=$(echo $state | grep -o "[0-9]:[0-9]*")
   
   NEXT_BATTERY_STATUS=" "
 
@@ -10,7 +10,7 @@ function battery_status_charging() {
 
 function battery_status_discharging() {
   local icons=(     )
-  local percentage=$(echo $state | rg -o "[0-9]*%" | rg -o "[0-9]*")
+  local percentage=$(echo $state | grep -o "[0-9]*%" | grep -o "[0-9]*")
   local segment=$(( ($percentage + 20 - 1) / 20 ))
 
   NEXT_BATTERY_STATUS="${icons[$segment]}"
@@ -22,8 +22,8 @@ function battery_status_discharging() {
 
 function battery_status() {
   local state=$(pmset -g batt)
-  local charging=$(echo $state | rg -o "\scharg[ing|ed]")
-  local discharging=$(echo $state | rg -o "\sdischarging")
+  local charging=$(echo $state | grep -o "\scharg[ing|ed]")
+  local discharging=$(echo $state | grep -o "\sdischarging")
 
   if [[ -n $charging ]]; then
     battery_status_charging
